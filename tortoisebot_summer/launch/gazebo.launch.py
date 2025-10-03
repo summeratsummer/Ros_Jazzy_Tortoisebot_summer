7695801514e12aa52792c7be4cfc787c2dd4daa8
summeratsummer <mikalinmonday2020@gmail.com>
2025-10-03 12:53:31 +0530

    Update gazebo.launch.py

--- tortoisebot_summer/launch/gazebo.launch.py
+++ tortoisebot_summer/launch/gazebo.launch.py
@@ -15,4 +15,18 @@ def generate_launch_description():
             ),
             launch_arguments={'world': os.path.join(pkg_tortoisebot, 'worlds', 'empty.world')}.items(),
         ),
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(
+                os.path.join(
+                    os.getenv('COLCON_PREFIX_PATH').split(':')[0],
+                    'share/tortoisebot_description/urdf/tortoisebot.urdf'
+                )).read()}]
+        )
     ])
