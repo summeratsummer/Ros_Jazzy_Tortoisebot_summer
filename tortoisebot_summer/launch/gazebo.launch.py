4f635b688062eeba7afffc80cb0ba64ff1b46d15
summeratsummer <mikalinmonday2020@gmail.com>
2025-10-03 12:48:56 +0530

    Add files via upload

---
+++ tortoisebot_summer/launch/gazebo.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_tortoisebot = get_package_share_directory('tortoisebot_description')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+            ),
+            launch_arguments={'world': os.path.join(pkg_tortoisebot, 'worlds', 'empty.world')}.items(),
+        ),
+    ])
